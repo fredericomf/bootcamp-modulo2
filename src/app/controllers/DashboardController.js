44a1b602bf1a050607073114f7bd95198dfb316c
Frederico Mottinha de Figueiredo <contato@fredericomf.com>
2018-12-03 20:50:58 -0200

    Test: Desafio 2.

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -1,10 +1,39 @@
-const { User } = require('../models')
+const { User, Appointment } = require('../models')
+const { Op } = require('sequelize')
+const moment = require('moment')
 
 class DashboardController {
   async index (req, res) {
-    const providers = await User.findAll({ where: { provider: true } })
+    const { provider, id } = req.session.user
 
-    return res.render('dashboard', { providers })
+    if (provider === true) {
+      const today = moment().add(1, 'days')
+
+      const appointmentsOfDay = await Appointment.findAll({
+        include: [{ model: User, as: 'user' }],
+        where: {
+          date: {
+            [Op.between]: [
+              today.startOf('day').format(),
+              today.endOf('day').format()
+            ]
+          },
+          provider_id: id
+        }
+      })
+
+      const appointments = appointmentsOfDay.map(appointment => {
+        return {
+          appointment,
+          formated_time: moment(appointment.date).format('HH:mm')
+        }
+      })
+
+      return res.render('dashboard_provider', { appointments })
+    } else {
+      const providers = await User.findAll({ where: { provider: true } })
+      return res.render('dashboard', { providers })
+    }
   }
 }
 
